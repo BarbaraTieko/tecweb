19190aaf1cd00a7dd0beb269d65a7ebeb6bad80e
Barbara Agena <barbara.tieko@gmail.com>
2022-08-24 22:53:58 -0300

    Adicionando aula 3

--- gabaritos/2022.2/Aula01/utils.py
+++ gabaritos/2022.2/Aula01/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+from urllib import response
 CUR_DIR = Path(__file__).parent
 
 def extract_route(request):
@@ -23,3 +24,18 @@ def load_template(arquivo):
     with open(filename, 'r') as file:
         conteudo = file.read()
     return conteudo
+
+def save_note(params):
+    notas = load_data("notes.json")
+    notas.append(params)
+
+    with open("data/notes.json", "w", encoding="utf8") as arquivo:
+        json.dump(notas, arquivo, ensure_ascii=False, indent=4)
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    if headers == "":
+        response = f"HTTP/1.1 {code} {reason}\n\n{body}"
+    else: 
+        response = f"HTTP/1.1 {code} {reason}\n{headers}\n\n{body}"
+
+    return response.encode()
--- gabaritos/2022.2/Aula01/views.py
+++ gabaritos/2022.2/Aula01/views.py
@@ -1,6 +1,24 @@
-from utils import load_data, load_template
+from utils import load_data, load_template, save_note
+from urllib.parse import unquote_plus
 
-def index():
+def index(request):
+    if request.startswith('POST'):
+        request = request.replace('\r', '')  # Remove caracteres indesejados
+        # Cabeçalho e corpo estão sempre separados por duas quebras de linha
+        partes = request.split('\n\n')
+        corpo = partes[1]
+        params = {}
+
+        # Preencha o dicionário params com as informações do corpo da requisição
+        # O dicionário conterá dois valores, o título e a descrição.
+        # Posteriormente pode ser interessante criar uma função que recebe a
+        # requisição e devolve os parâmetros para desacoplar esta lógica.
+        # Dica: use o método split da string e a função unquote_plus
+        for chave_valor in corpo.split('&'):
+            chave, valor = chave_valor.split("=")
+            params[chave] = unquote_plus(valor)
+            
+        save_note(params)
     # Cria uma lista de <li>'s para cada anotação
     # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
     note_template = load_template('components/note.html')
