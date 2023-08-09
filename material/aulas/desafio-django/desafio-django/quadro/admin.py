6dd14c3b5c755304b96ec071a4f26e8684dfeb71
BarbaraTieko <barbara_tieko@hotmail.com>
2023-08-09 16:04:28 -0300

    Removendo algumas aulas

--- material/aulas/desafio-django/desafio-django/quadro/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Tarefa, Pessoa, ResponsavelDaTarefa
-
-admin.site.register(Tarefa)
-admin.site.register(Pessoa)
-admin.site.register(ResponsavelDaTarefa)
